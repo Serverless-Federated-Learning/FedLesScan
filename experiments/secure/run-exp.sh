f126e4fe19c68cd46a2cb3d4270dae311510bc0a
Andreas Grafberger <andreasgrafberger@posteo.net>
2021-05-03 22:28:08 +0200

    adjust experimentation params

--- experiments/secure/run-exp.sh
+++ experiments/secure/run-exp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 python3 fedkeeper-mnist-secure.py --config config-secure.yaml \
-  --n-clients 50 \
-  --clients-per-round 10 \
-  --allowed-stragglers 2 \
+  --n-clients 100 \
+  --clients-per-round 25 \
+  --allowed-stragglers 5 \
   --accuracy-threshold 0.95
