5137bb460648322663596e4246f610f921c9a61c
Andreas Grafberger <andreasgrafberger@posteo.net>
2021-05-18 14:29:03 +0200

    updated fedless exp

--- experiments/fedkeeper/fedless-mnist-baseline.sh
+++ experiments/fedkeeper/fedless-mnist-baseline.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python3 fedless-mnist-baseline.py --config fedless-config.yaml \
+python3 fedless-mnist-baseline.py --config config-indep-gcloud.yaml \
   --n-clients 100 \
   --clients-per-round 100 \
   --allowed-stragglers 10 \
--- experiments/fedkeeper/shakespeare-baseline.sh
+++ experiments/fedkeeper/shakespeare-baseline.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-python3 fedless-shakespeare.py --config fedless-config-shakespeare.yaml \
-  --n-clients 40 \
-  --clients-per-round 20 \
+python3 fedless-shakespeare.py --config config-indep-gcloud-shakespeare.yaml \
+  --n-clients 30 \
+  --clients-per-round 30 \
   --allowed-stragglers 10 \
   --accuracy-threshold 0.80
