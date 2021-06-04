b0a924b4c6114af76ede19323d6f21dd0a97c1a7
Andreas Grafberger <andreasgrafberger@posteo.net>
2021-06-04 17:34:22 +0200

    fix shellcheck issues

--- experiments/fedkeeper/femnist-secure-large.sh
+++ experiments/fedkeeper/femnist-secure-large.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC2043
 for clients_in_round in 10; do
   python3 fedless-leaf.py --config config-indep-gcloud-secure-femnist.yaml \
     --dataset "femnist" \
