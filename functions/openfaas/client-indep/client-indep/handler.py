b41e0ffcc19cde6b9e3539cca40ec40b42c359b7
Andreas Grafberger <andreasgrafberger@posteo.net>
2021-06-20 18:01:44 +0200

    prototype

--- functions/openfaas/client-indep/client-indep/handler.py
+++ functions/openfaas/client-indep/client-indep/handler.py
@@ -1,9 +1,30 @@
-import fedless
-from fedless.models import TestMetrics
+import logging
+
+from pydantic import ValidationError
+
+from fedless.client import (
+    fedless_mongodb_handler,
+    ClientError,
+)
+from fedless.models import InvokerParams
+from fedless.providers import lambda_proxy_handler
+
+logging.basicConfig(level=logging.DEBUG)
 
 
+@lambda_proxy_handler(caught_exceptions=(ValidationError, ClientError))
 def handle(event, context):
-    return {
-        "statusCode": 200,
-        "body": f"Hello from OpenFaaS! {TestMetrics(cardinality=2, metrics={'acc': 2.0}).json()}",
-    }
+    """
+    Train client on given data and model and return :class:`fedless.client.ClientResult`.
+    Relies on :meth:`fedless.client.lambda_proxy_handler` decorator
+    for return object conversion and error handling
+    :return Response dictionary compatible with API gateway's lambda-proxy integration
+    """
+    config = InvokerParams.parse_obj(event["body"])
+
+    return fedless_mongodb_handler(
+        session_id=config.session_id,
+        round_id=config.round_id,
+        client_id=config.client_id,
+        database=config.database,
+    )
