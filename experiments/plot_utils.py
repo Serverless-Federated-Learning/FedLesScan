32ac071527fcbe6bfa94146e41fab7dd23fffa0e
Andreas Grafberger <grafbergerandreas@gmail.com>
2021-07-22 21:20:01 +0200

    update cost calculation and experiment config

--- experiments/plot_utils.py
+++ experiments/plot_utils.py
@@ -7,6 +7,114 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+GCLOUD_FUNCTION_TIERS = [
+    (128, 0.2, 0.000000231),
+    (256, 0.4, 0.000000463),
+    (512, 0.8, 0.000000925),
+    (1024, 1.4, 0.000001650),
+    (2048, 2.4, 0.000002900),
+    (4096, 4.8, 0.000005800),
+    (8192, 4.8, 0.000006800),
+]
+
+
+def calc_gcloud_function_cost(
+    memory,
+    cpu_ghz,
+    invocations,
+    function_runtime_seconds,
+    function_egress_mb,
+    substract_free_tier: bool = True,
+):
+    PRICE_EGRESS_PER_GB = 0.12
+    INVOCATIONS_UNIT_PRICE = 0.0000004
+    GB_SECOND_UNIT_PRICE = 0.0000025
+    GHZ_SECOND_UNIT_PRICE = 0.0000100
+    NETWORKING_UNIT_PRICE = 0.12
+
+    gb_sec_per_invoc = (memory / 1024.0) * function_runtime_seconds
+    ghz_sec_per_invoc = cpu_ghz * function_runtime_seconds
+    gb_sec_per_month = gb_sec_per_invoc * invocations
+    ghz_sec_per_month = ghz_sec_per_invoc * invocations
+    egress_gb_per_month = invocations * function_egress_mb / 1024
+
+    if substract_free_tier:
+        invocations = max(0, invocations - 2_000_000)
+        gb_sec_per_month = max(0, gb_sec_per_month - 400_000)
+        ghz_sec_per_month = max(0, ghz_sec_per_month - 200_000)
+        egress_gb_per_month = max(0, egress_gb_per_month - 5)
+
+    return (
+        invocations * INVOCATIONS_UNIT_PRICE
+        + gb_sec_per_month * GB_SECOND_UNIT_PRICE
+        + ghz_sec_per_month * GHZ_SECOND_UNIT_PRICE
+        + egress_gb_per_month * NETWORKING_UNIT_PRICE
+    )
+
+
+def calc_lambda_function_cost(
+    memory: float,
+    invocations: float,
+    function_runtime_seconds: float,
+    function_egress_mb: float,
+    num_active_instances: int,
+    substract_free_tier: bool = False,
+):
+    # LAMBDA PRICING
+    # Anforderungen	0,20 USD pro 1 Mio. Anforderungen
+    # Dauer	0,0000166667 USD für jede GB-Sekunde
+
+    # EGRESS
+    # 0,00 USD pro GB
+    # Nächste 9,999 TB/Monat	0,09 USD pro GB
+    # Nächste 40 TB/Monat	0,085 USD pro GB
+    # Nächste 100 TB/Monat	0,07 USD pro GB
+    # Mehr als 150 TB/Monat	0,05 USD pro GB
+
+    # Mit dem kostenlosen AWS-Nutzungskontingent erhalten Sie ein Jahr lang bis zu 15 GB ausgehende Datenübertragung
+    # Das kostenlose Kontingent für AWS Lambda umfasst 1 Mio. kostenlose Anforderungen pro Monat und 400 000 GB-Sekunden Datenverarbeitungszeit pro Monat.
+    # In calculation ignore ECR
+
+    # ECR
+    # ECR Image Size
+    # Übertragung ausgehender Daten **
+    # Bis zu 1 GB pro Monat	0,00 USD pro GB
+    # Nächste 9.999 TB pro Monat	0,09 USD pro GB
+    # Nächste 40 TB pro Monat	0,085 USD pro GB
+    # Nächste 100 TB pro Monat	0,07 USD pro GB
+    # Über 150 TB pro Monat	0,05 USD pro GB
+
+    DOLLARS_INVOCATIONS_PER_MILLION = 0.2
+    DOLLARS_GB_SEC = 0.0000166667
+    FREE_TIER_EGRESS_GB = 15.0
+    DOLLAR_EGRESS_PER_GB = 0.09
+    DOLLAR_ECR_EGRESS_PER_GB = 0.09
+    FEDLESS_IMAGE_SIZE_MB = 1529.67
+
+    total_gb_secs = (memory / 1024) * function_runtime_seconds * invocations
+    if substract_free_tier:
+        total_gb_secs = max(total_gb_secs - 400_000, 0.0)
+    invocations_in_millions = (invocations // 1_000_000) + (
+        1.0 if not substract_free_tier else 0.0
+    )
+    total_egress_gb = (function_egress_mb / 1024) * invocations
+    total_ecr_egress_gb = (FEDLESS_IMAGE_SIZE_MB / 1024) * num_active_instances
+
+    assert (
+        total_egress_gb < 9_999
+    ), "Total Egress (GB) {total_egress_gb} has to be below 10TB"
+
+    if substract_free_tier:
+        total_egress_gb = max(total_egress_gb - FREE_TIER_EGRESS_GB, 0.0)
+
+    # num_active_instances
+
+    egress_price = total_egress_gb * DOLLAR_EGRESS_PER_GB
+    invocation_costs = invocations_in_millions * DOLLARS_INVOCATIONS_PER_MILLION
+    runtime_costs = total_gb_secs * DOLLARS_GB_SEC
+    ecr_transfer_costs = total_ecr_egress_gb * DOLLAR_ECR_EGRESS_PER_GB
+    return invocation_costs + runtime_costs + egress_price + ecr_transfer_costs
+
 
 def read_flower_mnist_log_file(path: Path):
     lines = path.read_text().splitlines()
--- experiments/system/fedless-femnist-200-100-5-10-0001.sh
+++ experiments/system/fedless-femnist-200-100-5-10-0001.sh
@@ -8,24 +8,24 @@ root_directory="$(dirname "$(dirname "$script_dir")")"
 n_clients=200
 clients_per_round=100
 allowed_stragglers=10
-accuracy_threshold=0.99
+accuracy_threshold=0.9
 max_rounds=100
 
 # shellcheck disable=SC2034
-for i in {1..3}; do
+for i in {3..3}; do
     python3 -m fedless.benchmark.scripts \
-		-d "mnist" \
+		-d "femnist" \
 		-s "fedless" \
-		-c fedless-mnist-200-100-5-10-0001.yaml \
+		-c fedless-femnist-200-100-5-10-0001.yaml \
 		--clients "$n_clients" \
 		--clients-in-round "$clients_per_round" \
 		--stragglers "$allowed_stragglers" \
 		--rounds "$max_rounds" \
 		--max-accuracy "$accuracy_threshold" \
-		--out "$root_directory/out/fedless-mnist-200-100-5-10-0001" \
-		--aggregate-offline \
+		--out "$root_directory/out/fedless-femnist-200-100-5-10-0001" \
+		--aggregate-online \
 		--no-tum-proxy \
-		--timeout 60
+		--timeout 120
     sleep 610
 done
 
