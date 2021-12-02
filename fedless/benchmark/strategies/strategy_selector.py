7ff3a6e5f4352104b68c19e515efc1c4821e1116
mohamedazab <mohamedazab1996@gmail.com>
2021-12-02 19:55:24 +0000

    feat: add strat selector

---
+++ fedless/benchmark/strategies/strategy_selector.py
@@ -0,0 +1,16 @@
+from fedless.benchmark.strategies.fedkeeper import FedkeeperStrategy
+from fedless.benchmark.strategies.fedless_strategy import FedlessStrategy
+from fedless.mocks.mock_fedless_strategy import MockFedlessStrategy
+
+
+
+def selectStrategy(strategy:str, invocation_attrs:dict):
+    # todo fix fedkeeper args
+    switcher = {
+        # "fedkeeper": FedkeeperStrategy(**invocation_attrs),
+        "fedless": FedlessStrategy(**invocation_attrs),
+        "fedless_mock":MockFedlessStrategy(**invocation_attrs)
+    }
+
+    # default to fedless strategy
+    return switcher.get(strategy,switcher["fedless"])
