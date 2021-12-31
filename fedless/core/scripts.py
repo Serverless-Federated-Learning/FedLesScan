acaad69bba8566a97662a4e503c66bd852b7a960
mohamedazab <mohamedazab1996@gmail.com>
2021-12-31 21:42:06 +0000

    adding dataset runner

--- fedless/core/scripts.py
+++ fedless/core/scripts.py
@@ -1,22 +1,24 @@
 import asyncio
 import logging
-import sys
 import uuid
 from itertools import cycle
 from pathlib import Path
-from typing import List, Union, Tuple, Optional, Dict
+from typing import List, Union, Tuple, Dict
 
 import click
 import tensorflow as tf
 
 from fedless.core.common import parse_yaml_file
-from fedless.datasets.mnist.data_loader import create_mnist_train_data_loader_configs
-from fedless.datasets.mnist.model import create_mnist_cnn
-from fedless.datasets.leaf.model import create_femnist_cnn, create_shakespeare_lstm
 from fedless.core.models import (
     CognitoConfig,
     ExperimentConfig,
     FedkeeperClientsConfig,
+)
+from fedless.datasets.benchmark_configurator import (
+    create_model,
+    init_store_model,
+    create_mnist_test_config,
+    create_data_configs,
 )
 from fedless.strategies.Intelligent_selection import (
     DBScanClientSelection,
@@ -28,30 +30,15 @@ from fedless.models import (
     ClientPersistentHistory,
     MongodbConnectionConfig,
     DatasetLoaderConfig,
-    BinaryStringFormat,
-    SerializedParameters,
-    WeightsSerializerConfig,
-    NpzWeightsSerializerConfig,
-    LEAFConfig,
-    MNISTConfig,
 )
 from fedless.persistence.client_daos import (
     ClientConfigDao,
     ClientHistoryDao,
-    ParameterDao,
-    ModelDao,
 )
 from fedless.providers import OpenwhiskCluster
-from fedless.serialization import (
-    serialize_model,
-    NpzWeightsSerializer,
-    Base64StringConverter,
-)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-FILE_SERVER = "http://138.246.235.163:31715"
 
 
 @click.command()
@@ -253,37 +240,6 @@ def run(
     )
 
 
-def init_store_model(
-    session: str,
-    model: tf.keras.Sequential,
-    database: MongodbConnectionConfig,
-    store_json_serializable: bool = False,
-):
-    parameters_dao = ParameterDao(db=database)
-    models_dao = ModelDao(db=database)
-
-    serialized_model = serialize_model(model)
-    weights = model.get_weights()
-    weights_serialized = NpzWeightsSerializer(compressed=False).serialize(weights)
-    weights_format = BinaryStringFormat.NONE
-    if store_json_serializable:
-        weights_serialized = Base64StringConverter.to_str(weights_serialized)
-        weights_format = BinaryStringFormat.BASE64
-    params = SerializedParameters(
-        blob=weights_serialized,
-        serializer=WeightsSerializerConfig(
-            type="npz", params=NpzWeightsSerializerConfig(compressed=False)
-        ),
-        string_format=weights_format,
-    )
-    logger.debug(
-        f"Model loaded and successfully serialized. Total size is {sys.getsizeof(weights_serialized) // 10 ** 6}MB. "
-        f"Saving initial parameters to database"
-    )
-    parameters_dao.save(session_id=session, round_id=0, params=params)
-    models_dao.save(session_id=session, model=serialized_model)
-
-
 def store_client_configs(
     session: str,
     clients: FedkeeperClientsConfig,
@@ -339,59 +295,6 @@ def store_client_configs(
         f"Configured and stored all {len(data_configs)} clients configurations..."
     )
     return final_configs
-
-
-def create_model(dataset) -> tf.keras.Sequential:
-    if dataset.lower() == "femnist":
-        return create_femnist_cnn()
-    elif dataset.lower() == "shakespeare":
-        return create_shakespeare_lstm()
-    elif dataset.lower() == "mnist":
-        return create_mnist_cnn()
-    else:
-        raise NotImplementedError()
-
-
-def create_mnist_test_config(proxies) -> DatasetLoaderConfig:
-    return DatasetLoaderConfig(
-        type="mnist", params=MNISTConfig(split="test", proxies=proxies)
-    )
-
-
-# noinspection PydanticTypeChecker,PyTypeChecker
-def create_data_configs(
-    dataset: str, clients: int, proxies: Optional[Dict] = None
-) -> List[Union[DatasetLoaderConfig, Tuple[DatasetLoaderConfig, DatasetLoaderConfig]]]:
-    dataset = dataset.lower()
-    if dataset == "mnist":
-        return list(
-            create_mnist_train_data_loader_configs(
-                n_devices=clients, n_shards=600, proxies=proxies
-            )
-        )
-    elif dataset in ["femnist", "shakespeare"]:
-        configs = []
-        for client_idx in range(clients):
-            train = DatasetLoaderConfig(
-                type="leaf",
-                params=LEAFConfig(
-                    dataset=dataset,
-                    location=f"{FILE_SERVER}/data/leaf/data/{dataset}/data/"
-                    f"train/user_{client_idx}_train_9.json",
-                ),
-            )
-            test = DatasetLoaderConfig(
-                type="leaf",
-                params=LEAFConfig(
-                    dataset=dataset,
-                    location=f"{FILE_SERVER}/data/leaf/data/{dataset}/data/"
-                    f"test/user_{client_idx}_test_9.json",
-                ),
-            )
-            configs.append((train, test))
-        return configs
-    else:
-        raise NotImplementedError(f"Dataset {dataset} not supported")
 
 
 if __name__ == "__main__":
