bc89d5f4b5e583575da6953315a1fffd9070c638
mohamedazab <mohamedazab1996@gmail.com>
2022-01-06 19:56:25 +0000

    add leaf fmnist config

--- experiment/run-femnist-demo.sh
+++ experiment/run-femnist-demo.sh
@@ -7,23 +7,24 @@ echo $root_directory
 n_clients=5
 clients_per_round=3
 allowed_stragglers=2
-accuracy_threshold=0.6
+accuracy_threshold=0.5
 rounds=10
 
 # shellcheck disable=SC2034
 for curr_repeat in {1..1}; do
   python -m fedless.core.scripts \
-    -d "mnist" \
+    -d "femnist" \
     -s "fedless_mock" \
-    -c "$script_dir/mnist-demo.yaml" \
+    -c "$script_dir/femnist-demo.yaml" \
     --clients "$n_clients" \
     --clients-in-round "$clients_per_round" \
     --stragglers "$allowed_stragglers" \
     --max-accuracy "$accuracy_threshold" \
-    --out "$root_directory/out/fedkeeper-mnist-demo" \
+    --out "$root_directory/out/femnist-demo" \
     --rounds "$rounds" \
-    --timeout 90000 \
-    --separate-invokers \
+    --aggregate-online \
+	--no-tum-proxy \
+	--timeout 120
 
   sleep 600
   exit 0
