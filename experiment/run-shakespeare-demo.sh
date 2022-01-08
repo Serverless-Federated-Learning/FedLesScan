24f21f3dc42afb6e88b7dd7ebe34a73ea210228e
mohamedazab <mohamedazab1996@gmail.com>
2022-01-08 00:45:03 +0000

    exp: add new exp configs for leaf

--- experiment/run-shakespeare-demo.sh
+++ experiment/run-shakespeare-demo.sh
@@ -5,7 +5,7 @@ root_directory="$(dirname "$(dirname "$script_dir")")"
 echo $script_dir
 echo $root_directory
 n_clients=10
-clients_per_round=3
+clients_per_round=5
 allowed_stragglers=2
 accuracy_threshold=0.5
 rounds=10
@@ -13,14 +13,14 @@ rounds=10
 # shellcheck disable=SC2034
 for curr_repeat in {1..1}; do
   python -m fedless.core.scripts \
-    -d "femnist" \
-    -s "fedless" \
-    -c "$script_dir/femnist-demo.yaml" \
+    -d "shakespeare" \
+    -s "fedless_mock" \
+    -c "$script_dir/shakespeare-demo.yaml" \
     --clients "$n_clients" \
     --clients-in-round "$clients_per_round" \
     --stragglers "$allowed_stragglers" \
     --max-accuracy "$accuracy_threshold" \
-    --out "$root_directory/out/femnist-demo" \
+    --out "$root_directory/out/shakespeare-demo" \
     --rounds "$rounds" \
     --aggregate-online \
 	--no-tum-proxy \
