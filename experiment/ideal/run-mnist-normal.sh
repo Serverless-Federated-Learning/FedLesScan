c03ba8f8e2aad2f1235884e7eb23e5cce6009c5f
mohamedazab <mohamedazab1996@gmail.com>
2022-03-06 22:26:11 +0000

    fix: mnist exp config

--- experiment/ideal/run-mnist-normal.sh
+++ experiment/ideal/run-mnist-normal.sh
@@ -6,12 +6,27 @@ echo $script_dir
 echo $root_directory
 n_clients=300
 clients_per_round=100
-allowed_stragglers=10
+allowed_stragglers=100
 accuracy_threshold=0.99
 rounds=80
 
 # shellcheck disable=SC2034
 # for curr_repeat in {1..1}; do
+python -m fedless.core.scripts \
+  -d "mnist" \
+  -s "fedless_enhanced" \
+  -c "$script_dir/mnist-demo.yaml" \
+  --clients "$n_clients" \
+  --clients-in-round "$clients_per_round" \
+  --stragglers "$allowed_stragglers" \
+  --max-accuracy "$accuracy_threshold" \
+  --out "$root_directory/out/ideal2/mnist-enhanced" \
+  --rounds "$rounds" \
+  --timeout 90000 \
+  --mock 
+
+sleep 2
+
 python -m fedless.core.scripts \
   -d "mnist" \
   -s "fedless" \
@@ -25,20 +40,7 @@ python -m fedless.core.scripts \
   --timeout 90000 \
   --mock 
 
-sleep 2
 
-python -m fedless.core.scripts \
-  -d "mnist" \
-  -s "fedless_enhanced" \
-  -c "$script_dir/mnist-demo.yaml" \
-  --clients "$n_clients" \
-  --clients-in-round "$clients_per_round" \
-  --stragglers "$allowed_stragglers" \
-  --max-accuracy "$accuracy_threshold" \
-  --out "$root_directory/out/ideal2/mnist-enhanced" \
-  --rounds "$rounds" \
-  --timeout 90000 \
-  --mock 
 exit 0
 
 # done
