4003fde43dac03e375a80b685b40f2df4a637817
mohamedazab <mohamedazab1996@gmail.com>
2022-03-11 08:48:15 +0100

    add ofaas aggregator function

---
+++ functions/openfaas/aggregator/aggregator/handler.py
@@ -0,0 +1,29 @@
+import logging
+
+from pydantic import ValidationError
+
+
+from fedless.model_aggregation.aggregation import (
+    default_aggregation_handler,
+    AggregationError,
+)
+from fedless.models import AggregatorFunctionParams
+from fedless.providers import openfaas_action_handler
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+@openfaas_action_handler(caught_exceptions=(ValidationError, AggregationError))
+def handle(event, context):
+    config = AggregatorFunctionParams.parse_raw(event.body)
+
+    return default_aggregation_handler(
+        session_id=config.session_id,
+        round_id=config.round_id,
+        database=config.database,
+        serializer=config.serializer,
+        online=config.online,
+        test_data=config.test_data,
+        test_batch_size=config.test_batch_size,
+        aggregation_strategy=config.aggregation_strategy,
+    )
