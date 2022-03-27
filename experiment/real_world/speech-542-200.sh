ad1284e08b6d3cd6e22eb9a185afce852eea3e3a
mohamedazab <mohamedazab1996@gmail.com>
2022-03-27 10:11:59 +0000

    add speech exp config

--- experiment/real_world/speech-542-200.sh
+++ experiment/real_world/speech-542-200.sh
@@ -8,28 +8,28 @@ n_clients=542
 clients_per_round=200
 allowed_stragglers=200
 accuracy_threshold=0.99
-rounds=35
+rounds=60
 dataset_name="speech"
 client_timeout=35
 
-base_out_dir="$root_directory/out/real_world/linear"
+base_out_dir="$root_directory/out/real_world/expo/"
 config_dir="$script_dir/$dataset_name-$n_clients-$clients_per_round.yaml"
 echo $base_out_dir
 # shellcheck disable=SC2034
-for straggler_percent in 0.1 ; do
+for straggler_percent in 0.7 ; do
   
-  # python -m fedless.core.scripts \
-  #   -d "$dataset_name" \
-  #   -s "fedless" \
-  #   -c "$config_dir" \
-  #   --clients "$n_clients" \
-  #   --clients-in-round "$clients_per_round" \
-  #   --stragglers "$allowed_stragglers" \
-  #   --max-accuracy "$accuracy_threshold" \
-  #   --out "$base_out_dir/$dataset_name-$straggler_percent" \
-  #   --rounds "$rounds" \
-  #   --timeout "$client_timeout" \
-  #   --simulate-stragglers "$straggler_percent"
+  python -m fedless.core.scripts \
+    -d "$dataset_name" \
+    -s "fedless" \
+    -c "$config_dir" \
+    --clients "$n_clients" \
+    --clients-in-round "$clients_per_round" \
+    --stragglers "$allowed_stragglers" \
+    --max-accuracy "$accuracy_threshold" \
+    --out "$base_out_dir/$dataset_name-$straggler_percent" \
+    --rounds "$rounds" \
+    --timeout "$client_timeout" \
+    --simulate-stragglers "$straggler_percent"
   
   sleep 1
 
@@ -49,19 +49,19 @@ for straggler_percent in 0.1 ; do
   sleep 1
 
 
-  # python -m fedless.core.scripts \
-  #   -d "$dataset_name" \
-  #   -s "fedprox" \
-  #   -c "$config_dir" \
-  #   --clients "$n_clients" \
-  #   --clients-in-round "$clients_per_round" \
-  #   --stragglers "$allowed_stragglers" \
-  #   --max-accuracy "$accuracy_threshold" \
-  #   --out "$base_out_dir/$dataset_name-prox-$straggler_percent" \
-  #   --rounds "$rounds" \
-  #   --timeout "$client_timeout" \
-  #   --simulate-stragglers "$straggler_percent" \
-  #   --mu 0.001
+  python -m fedless.core.scripts \
+    -d "$dataset_name" \
+    -s "fedprox" \
+    -c "$config_dir" \
+    --clients "$n_clients" \
+    --clients-in-round "$clients_per_round" \
+    --stragglers "$allowed_stragglers" \
+    --max-accuracy "$accuracy_threshold" \
+    --out "$base_out_dir/$dataset_name-prox-$straggler_percent" \
+    --rounds "$rounds" \
+    --timeout "$client_timeout" \
+    --simulate-stragglers "$straggler_percent" \
+    --mu 0.001
 
 done
 
