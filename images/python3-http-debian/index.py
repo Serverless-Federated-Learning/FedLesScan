31c92814bf9588015da4b5b454cb4ebf47d3897b
mohamedazab <mohamedazab1996@gmail.com>
2022-04-04 12:31:38 +0000

    add evaluator to function in openfaas

--- images/python3-http-debian/index.py
+++ images/python3-http-debian/index.py
@@ -89,10 +89,4 @@ def call_handler(path):
 
 
 if __name__ == "__main__":
-    serve(
-        app,
-        host="0.0.0.0",
-        port=5000,
-        threaded=False,
-        threads=0,
-    )
+    serve(app, host="0.0.0.0", port=5000, threads=1, connection_limit=40)
