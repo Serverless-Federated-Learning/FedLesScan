f2d308d60fe6fc19a76afb88206f3986a6bdd063
mohamedazab <mohamedazab1996@gmail.com>
2022-06-18 12:02:14 +0200

    remove test server

--- fedless/common/models/models.py
+++ fedless/common/models/models.py
@@ -132,8 +132,8 @@ class MongodbConnectionConfig(BaseSettings):
     @property
     def url(self) -> str:
         """Return url representation"""
-        # return f"mongodb://{parse.quote(self.username)}:{parse.quote(self.password)}@{self.host}:{self.port}"        
-        return f"mongodb+srv://ml96:ml96@cluster0.ucq8wni.mongodb.net/?retryWrites=true&w=majority"
+        return f"mongodb://{parse.quote(self.username)}:{parse.quote(self.password)}@{self.host}:{self.port}"        
+        
 
 
     class Config:
